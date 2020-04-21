84b74319a161241c40c101c1166a605ec720e901
lucaskahl <lucaschangkahl@gmail.com>
2020-04-21 19:03:16 -0300

    Added TypeORM config, reflect-metadata, migration and Changed methods

--- node-js/src/services/CreateAppointmentService.ts
+++ node-js/src/services/CreateAppointmentService.ts
@@ -1,4 +1,6 @@
+import { getCustomRepository } from 'typeorm';
 import { startOfHour } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import ApppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,16 +10,12 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: ApppointmentsRepository;
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(ApppointmentsRepository);
 
-  constructor(appointmentsRepository: ApppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -25,10 +23,12 @@ class CreateAppointmentService {
       throw Error('This appointment date already owned');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
