4abbe8ba050b0392ca4c7f95fdcdf771058fd44a
lucaskahl <lucaschangkahl@gmail.com>
2020-06-02 23:28:29 -0300

    Added project fixes and forgotten features

--- node-js/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ node-js/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-
+import { classToClass } from 'class-transformer';
 import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
@@ -7,7 +7,7 @@ import ListProviderAppointmentsService from '@modules/appointments/services/List
 export default class ProviderAppointmentsController {
   public async index(req: Request, res: Response): Promise<Response> {
     const provider_id = req.user.id;
-    const { day, month, year } = req.body;
+    const { day, month, year } = req.query;
 
     const listProviderAppointments = container.resolve(
       ListProviderAppointmentsService,
@@ -15,11 +15,11 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return res.json(appointments);
+    return res.json(classToClass(appointments));
   }
 }
